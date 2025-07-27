1b18578f8ed2f127bbd1cc0ea4554bf1233af93d
NokeshK <nokesh8919500709@gmail.com>
2025-07-27 16:06:29 +0530

    Updated UI1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <h1>Welcome to Our Website</h1>
-    </div>
+    <>
+      <div className="matrix-rain"></div>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Welcome to CS 213 Class!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Welcome to our amazing CS 213 CI/CD PROJECT !
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
